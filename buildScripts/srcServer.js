ae586774a0d48f14c3737081ceda834c22b2c879
John Noriega <johnnoriega@Johns-MacBook-Pro.local>
2019-08-23 19:34:21 -0500

    added boilerplate code

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import path from 'path';
+import open from 'open';
+
+const port = 3001;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
