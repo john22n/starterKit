e50eb3a75ca28e34950f7a83f0cb0c4cc8dc9f98
John Noriega <johnnoriega@Johns-MacBook-Pro.local>
2019-09-03 18:06:41 -0500

    index test file changed

--- src/index.test.js
+++ src/index.test.js
@@ -2,13 +2,15 @@ import jsdom from 'jsdom';
 import fs from 'fs';
 
 describe('index.html', () => {
-  expect('it to say something', () => {
-    const index = fs.readFileSync('./index.html', 'utf-8');
-    jsdon.env(index, (err, window) => {
-      const h1 = window.document.getElementById('yo')[0];
-      expect(h1.innerHTML).toEqual('yo');
-      done();
-      window.close();
+  it('should say something', () => {
+    expect('it to say something', () => {
+      const index = fs.readFileSync('./index.html', 'utf-8');
+      jsdom.env(index, (err, window) => {
+        const h1 = window.document.getElementById('yo')[0];
+        expect(h1.innerHTML).toEqual('yo');
+        done();
+        window.close();
+      })
     })
   })
-})
+});
